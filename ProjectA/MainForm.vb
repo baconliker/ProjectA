8829dc3e6c045b15c8772a9a459ae4b12fc829a2
Colin Baker <colin@colinbaker.me.uk>
2017-08-17 14:55:04 +0100

    letter 'd' added

--- ProjectA/MainForm.vb
+++ ProjectA/MainForm.vb
@@ -2,7 +2,7 @@
 
     Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
 
-        MessageBox.Show("Test complete.", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("Test completed.", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
