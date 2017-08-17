44e75bf3a758046076f16719e50339c054b5e7f2
Colin Baker <colin@colinbaker.me.uk>
2017-08-17 14:25:43 +0100

    Use messagebox to show message to user

--- ProjectA/MainForm.vb
+++ ProjectA/MainForm.vb
@@ -1,3 +1,9 @@
 ﻿Public Class MainForm
 
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+
+        MessageBox.Show("Test complete.", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+
 End Class
