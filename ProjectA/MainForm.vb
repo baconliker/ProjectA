adb2c16fc48540a545af9d8b72d0c259f8b009d2
Colin Baker <colin@colinbaker.me.uk>
2017-08-23 16:48:06 +0100

    Bertie message

--- ProjectA/MainForm.vb
+++ ProjectA/MainForm.vb
@@ -2,7 +2,7 @@
 
     Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
 
-        MessageBox.Show("Bill", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("Bertie", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
