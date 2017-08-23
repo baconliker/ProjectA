b3b0fccfefe99601e4bc58e6b04e567ff3157904
Colin Baker <colin@colinbaker.me.uk>
2017-08-23 15:54:06 +0100

    Change D

--- ProjectA/MainForm.vb
+++ ProjectA/MainForm.vb
@@ -2,7 +2,7 @@
 
     Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
 
-        MessageBox.Show("A", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("D", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
