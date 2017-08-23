5d0ffb12319c79432d035351211056791a0a4e58
Colin Baker <colin@colinbaker.me.uk>
2017-08-23 17:10:06 +0100

    Andy message

--- ProjectA/MainForm.vb
+++ ProjectA/MainForm.vb
@@ -2,7 +2,7 @@
 
     Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
 
-        MessageBox.Show("Alf", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("Andy", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
